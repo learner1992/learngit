376e058799716666f7c19e203e4dff746f5c2a93
learner1992 <makebelieving@163.com>
2018-08-30 19:49:21 +0800

    08301

--- python/common/util/log_util.py
+++ python/common/util/log_util.py
@@ -5,3 +5,9 @@ Created on 2018/8/30 19:33
 Copyright (c) 2018/8/30.
 @author: ogc
 """
+import content,logging
+from logging.handlers import TimedRotatingFileHandler
+
+class LogUtil:
+    base_logger=content._NULL_STR
+    log_dict={}
